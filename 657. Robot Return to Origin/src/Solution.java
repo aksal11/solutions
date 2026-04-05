73bbb008d38190815d8be0146688dee9a3c5dee2
aks4l <aksalmanoj11@gmail.com>
2026-04-05 21:18:02 +0530

    657. Robot Return to Origin

---
+++ 657. Robot Return to Origin/src/Solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+
+    public static void main(String[] args) {
+
+        String moves = "UD";
+        System.out.println(judgeCircle(moves));
+
+    }
+
+    public static boolean judgeCircle(String moves){
+
+//        coordinates
+        int x = 0;
+        int y =0;
+
+        for ( char move: moves.toCharArray()){
+
+            if ( move == 'U'){
+                y++;
+            }else if ( move == 'D'){
+                y--;
+            }else if ( move == 'R'){
+                x++;
+            }else if (move == 'L'){
+                y--;
+            }
+        }
+
+        return ( x ==0  && y == 0);
+    }
+}
