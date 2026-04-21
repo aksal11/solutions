db2fe9aee56df2b8c20ad76b5e66365fcd6d69ce
aks44l <aksalmanoj11@gmail.com>
2026-04-21 22:59:21 +0530

    27. remove element

---
+++ 27. Remove Element/src/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+
+    static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+        System.out.println(removeElement(nums,val));
+
+    }
+
+    private static int removeElement(int[] nums, int val){
+
+        int k = 0 ;
+
+        for ( int i = 0 ; i < nums.length; i++){
+            if ( nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
