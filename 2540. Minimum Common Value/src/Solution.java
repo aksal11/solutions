b79bea80964bef994eba63146652fcd487f70fde
aks44l <aksalmanoj11@gmail.com>
2026-05-19 21:04:13 +0530

    2540. Minimum Common Value

---
+++ 2540. Minimum Common Value/src/Solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+
+    public static void main() {
+
+        int[] nums1 = {1,2,3,6};
+        int[] nums2 = {2,3,4,5};
+        System.out.println(getCommon(nums1,nums2));
+
+
+    }
+
+    private static int getCommon(int[] nums1, int[] nums2){
+
+        int i = 0 ;
+        int j = 0;
+
+        while ( i < nums1.length && j < nums2.length){
+
+            if ( nums1[i] == nums2[j]){
+                return nums1[i];
+            }
+
+            if ( nums1[i] < nums2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+
+        return -1;
+    }
+}
