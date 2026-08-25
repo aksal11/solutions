1017d8f407682774382c25ccb0ef9681dd137d6c
aks44l <aksalmanoj11@gmail.com>
2026-08-25 23:13:02 +0530

    3718. smallest missing multiple of k

---
+++ 3718. Smallest Missing Multiple of K/src/Solution.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        int[] nums = {
+                1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
+                26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,
+                51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,
+                76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100};
+
+        int k = 1;
+
+        System.out.println(missingMultiple(nums,k));
+
+    }
+
+    private static int missingMultiple(int[] nums, int k){
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for ( int num : nums){
+            set.add(num);
+        }
+
+        int multiple =k;
+        while (set.contains(multiple)) {
+
+            multiple += k;
+        }
+        return  multiple;
+    }
+}
