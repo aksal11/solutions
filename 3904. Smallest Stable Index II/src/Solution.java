11d5e40b1f406cbf8e9f62df9e91753b194b6604
aks44l <aksalmanoj11@gmail.com>
2026-09-05 23:49:14 +0530

    3904. smallest stable index II

---
+++ 3904. Smallest Stable Index II/src/Solution.java
@@ -0,0 +1,40 @@
+public class Solution {
+
+    public static void main(String[] args) {
+
+        int[] nums = {5,0,1,4 };
+        int k = 3;
+        System.out.println(firstStableIndex(nums,k));
+
+    }
+
+    private static int firstStableIndex(int[] nums, int k){
+
+        int n = nums.length;
+
+        int[] prefixMax = new int[n];
+        int[] suffixMin = new int[n];
+
+        prefixMax[0] = nums[0];
+
+        for (int i = 1; i < n; i++){
+            prefixMax[i] = Math.max(prefixMax[i-1],nums[i] );
+        }
+
+        suffixMin[n-1] = nums[n-1];
+
+        for (int i = n - 2; i >= 0 ; i--){
+            suffixMin[i] = Math.min(suffixMin[i+1],nums[i] );
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            int instability = prefixMax[i] - suffixMin[i];
+
+            if ( instability <= k){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
